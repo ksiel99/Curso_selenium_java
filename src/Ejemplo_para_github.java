4cd863e714724cf8daf3acc07e8bd402bbc52986
Nicolas Alvarez <alvarez99@gmail.com>
2023-06-08 21:58:16 -0600

    Update Ejemplo_para_github.java
    actualizacion de clase ejemplo para github

--- src/Ejemplo_para_github.java
+++ src/Ejemplo_para_github.java
@@ -3,7 +3,7 @@ public class Ejemplo_para_github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// esta es una modificacion al archivo de ejemplo github
 	}
 
 }
