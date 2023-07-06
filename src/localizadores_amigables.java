b0a86f8bad5451f2c3e7d1b9b995158166530e55
Nicolas Alvarez <alvarez99@gmail.com>
2023-07-06 10:35:29 -0600

    update para firefox y chrome
    firefox y chrome update

--- src/localizadores_amigables.java
+++ src/localizadores_amigables.java
@@ -13,12 +13,31 @@ public class localizadores_amigables {
 		  WebDriver driver_edge = new EdgeDriver();
 		  //metodo basico 1, get = nos abre la pagina requerida
 		  driver_edge.get("https://www.google.com");
+		  //localizador relativo - derecha de
 		 // driver_edge.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.className("gb_v"))).click();
+		 //localizador relativo - concatenacion, derecha de e izquierda de
 		  driver_edge.findElement(RelativeLocator.with(By.tagName("a")).toLeftOf(By.className("gb_d")).toRightOf(By.className("gb_v"))).click();
 		  
 		//chrome
-		  
-		//firefox  
+		  System.setProperty("webdriver.Chrome.driver", "E:\\Canal Nicolas Alvarez\\nuevo curso avanzado de selenium con java\\drivers\\chromedrvier\\chromedriver.exe");
+		  WebDriver driver_chrome = new ChromeDriver();
+		  //metodo basico 1, get = nos abre la pagina requerida
+		  driver_chrome.get("https://www.google.com");
+		  //localizador relativo - derecha de
+		 // driver_chrome.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.className("gb_v"))).click();
+		 //localizador relativo - concatenacion, derecha de e izquierda de
+		  driver_chrome.findElement(RelativeLocator.with(By.tagName("a")).toLeftOf(By.className("gb_d")).toRightOf(By.className("gb_v"))).click();
+		   
+		//firefox 
+		  System.setProperty("webdriver.gecko.driver", "E:\\Canal Nicolas Alvarez\\nuevo curso avanzado de selenium con java\\drivers\\geckodriver\\geckodriver.exe");
+		  WebDriver driver_firefox = new FirefoxDriver();
+		  //metodo basico 1, get = nos abre la pagina requerida
+		  driver_firefox.get("https://www.google.com");
+		  //localizador relativo - derecha de
+		 // driver_firefox.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.className("gb_v"))).click();
+		 //localizador relativo - concatenacion, derecha de e izquierda de
+		  driver_firefox.findElement(RelativeLocator.with(By.tagName("a")).toLeftOf(By.className("gb_d")).toRightOf(By.className("gb_v"))).click();
+		   
 	}
 
 }
