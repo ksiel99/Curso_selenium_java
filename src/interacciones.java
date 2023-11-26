4be2fe13870cb51b838eaf50139405cd54e58ebf
Nicolas Alvarez <alvarez99@gmail.com>
2023-11-25 22:27:00 -0600

    update

--- src/interacciones.java
+++ src/interacciones.java
@@ -3,22 +3,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class interacciones {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.edge.driver", "E:\\Canal Nicolas Alvarez\\nuevo curso avanzado de selenium con java\\drivers\\edgedriver\\msedgedriver.exe");
-		WebDriver driver_edge = new EdgeDriver();
-		driver_edge.get("https://www.amazon.com");
-		//send keys 
-		driver_edge.findElement(By.id("twotabsearchtextbox")).sendKeys("nintendo switch");
-	    //driver_edge.findElement(By.id("nav-search-submit-button")).click();
-		//clear
-		driver_edge.findElement(By.id("twotabsearchtextbox")).clear();
-		//click()
-		driver_edge.findElement(By.id("nav-hamburger-menu")).click();
-		//submit() ya no es valido
+		System.setProperty("webdriver.chrome.driver", "E:\\Canal Nicolas Alvarez\\nuevo curso avanzado de selenium con java\\drivers\\chromedrvier\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("debuggerAddress", "127.0.0.1:9222");
+		WebDriver driver = new ChromeDriver(options);
+		System.out.println(driver.getTitle());
 	}
 
 }
--- src/navegacion.java
+++ src/navegacion.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 public class navegacion {
 
 	public static void main(String[] args) {
